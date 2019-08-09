887aa25b8ff715511bc62fe67243d1d30d368431
skarev <sevenkss95@gmail.com>
2019-08-09 22:01:18 +0300

    complete app

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -29,8 +29,39 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             imageView.image = userPickedImage
+            
+            guard let ciimage = CIImage(image: userPickedImage) else {
+                fatalError("Could not convert to CIImage.")
+            }
+            detect(image: ciimage)
         }
         imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func detect(image: CIImage){
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading CoreML Model Failed.")
+        }
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image.")
+            }
+            if let firstResult = results.first {
+                if firstResult.identifier.contains("hotdog"){
+                    self.navigationItem.title = "Hotdog!"
+                }else{
+                    self.navigationItem.title = "Not Hotdog!"
+                }
+            }
+        }
+        let handler = VNImageRequestHandler(ciImage: image)
+        do {
+            try handler.perform([request])
+        }catch{
+            print(error)
+        }
+        
     }
     
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
